d6ec076883a835cea151ec2db14d570976411416
Lee Raj <lerajk44@gmail.com>
2018-03-20 14:35:33 -0400

    changes to token

--- contracts/FieldToken.sol
+++ contracts/FieldToken.sol
@@ -1,21 +1,23 @@
 pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
-import "zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
 
 
-contract FieldToken is StandardToken, BurnableToken {
+contract FieldToken is StandardToken {
 
-	  string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public supply;
+  string public constant name = "FieldToken"; // solium-disable-line uppercase
+  string public constant symbol = "FLC"; // solium-disable-line uppercase
+  uint8 public constant decimals = 18; // solium-disable-line uppercase
 
-  	function FieldToken(string _name, string _symbol, uint8 _decimals, uint256 _supply) public {
-    	name = _name;
-    	symbol = _symbol;
-    	decimals = _decimals;
-    	supply = _supply;
-  	}
+  uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(decimals));
+
+  /**
+   * @dev Constructor that gives msg.sender all of existing tokens.
+   */
+  function FieldToken() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+    Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+  }
 
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var FieldToken = artifacts.require("./FieldToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(FieldToken, "FieldCoin", "FLC",18, 1000000000);
+  deployer.deploy(FieldToken);
 };
